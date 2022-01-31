2a4c9579ca538a7c82e5b57806803969df6ce525
alessandrotesta-99 <alessandro.testa@studenti.unicam.it>
2022-02-01 00:34:10 +0100

    first

---
+++ cmake-build-debug/CMakeFiles/AlessandroTesta105380.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AlessandroTesta105380.exe"
+  "AlessandroTesta105380.pdb"
+  "CMakeFiles/AlessandroTesta105380.dir/main.c.o"
+  "libAlessandroTesta105380.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AlessandroTesta105380.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/AlessandroTesta105380.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for AlessandroTesta105380.
+# This may be replaced when dependencies are built.
