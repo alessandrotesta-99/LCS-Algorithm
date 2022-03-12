753400235dfb295797dd545c6e89bf61384f2434
alessandrotesta-99 <alessandro.testa@studenti.unicam.it>
2022-03-12 20:09:01 +0100

    test main

--- main.c
+++ main.c
@@ -1,13 +1,23 @@
 #include "FileIO.h"
 #include "LCSAlgorithm.h"
+#include "MatrixLcs.h"
+#include <string.h>
 #include <stdlib.h>
 
+
+void exctractlcs(char **argv) {
+    char *result = read_file(argv[1]);
+    char *result2 = read_file(argv[2]);
+    char *output = NULL;
+    output = printLcs(result, result2);
+    write_file(argv[3], output);
+    free(output);
+    free(result);
+    free(result2);
+}
 int main(int argc, char **argv) {
-    char* output;
         if (argc == 4) {
-            output = printLcs(read_file(argv[1]), read_file(argv[2]));
-            write_file(argv[3], output);
-            free(output);
+            exctractlcs(argv);
         }
         else{
             printf("Incorrect number of arguments!");
